ac4d3993006be19d4e249b4863bcbb8c65386672
WizWiz <omidnaderivip@gmail.com>
2023-03-04 17:40:43 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+//telegram: @wizwizdev
+date_default_timezone_set('Asia/Tehran');
+set_time_limit(0);
+error_reporting(E_ALL);
+
+//------------------ Configurations ------------------ //
+$Config = [
+    'api_token' => "",
+    'admin' => [],
+    'report_channel' => -1000000 // -100xxxxxxx
+];
+$Database = [
+    'dbname' => "",
+    'username' => "",
+    'password' => ''
+];
+
+?>
