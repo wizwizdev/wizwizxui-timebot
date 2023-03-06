f29d7882ba11280f9a2be8ad0e1a7dece82141ed
WizWiz <omidnaderivip@gmail.com>
2023-03-06 23:59:51 +0330

    Update createDB.php

--- createDB.php
+++ createDB.php
@@ -6,10 +6,7 @@ $connection = mysqli_connect('localhost', $Database['username'], $Database['pass
 // ------------------ { Connect MySQL & Creat Table } ------------------ //
 $res = $connection->query("CREATE TABLE IF NOT EXISTS `user` (
     `id` bigint(10) NOT NULL PRIMARY KEY,
-    `step` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-    `uuid` varchar(1000),
-    `sub_server` varchar(1000),
-    `warned` varchar(100)
+    `step` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
     )");
 
 
@@ -19,6 +16,16 @@ $connection->query("CREATE TABLE IF NOT EXISTS `servers` (
     `user_name` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
     `password` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
     `type` varchar(100),
+    PRIMARY KEY (`id`)
+    )");
+
+$connection->query("CREATE TABLE IF NOT EXISTS `loged_info` (
+    `id` int(255) NOT NULL AUTO_INCREMENT,
+    `user_id` bigint(10) NOT NULL,
+    `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+    `uuid` varchar(1000),
+    `sub_server` varchar(1000),
+    `warned` varchar(100),
     PRIMARY KEY (`id`)
     )");
 
