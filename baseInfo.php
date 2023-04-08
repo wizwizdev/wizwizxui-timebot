8d41e96f8af7917d74a26f4345321bc9f5cd6e1a
WizWiz <omidnaderivip@gmail.com>
2023-04-08 18:16:24 +0000

    Update installation settings

---
+++ baseInfo.php
@@ -0,0 +1,14 @@
+<?php 
+	error_reporting(0);
+	$botToken = '[BOTTOKEN]';
+	$dbUserName = '[DBUSERNAME]';
+	$dbPassword = '[DBPASSWORD]';
+	$dbName = '[DBNAME]';
+	$admin = [ADMIN];
+	$channelLock = "[CHANNELLOCK]";
+	$botUrl = "[BOTURL]";
+	$walletwizwiz = "[WALLET]";
+	$pursantPercent = [PURSANT];
+    $nowPaymentKey = "[NOWPAYMENTKEY]";
+    $zarinpalId = "[ZARINPALKEY]";
+?>
