013bc2aa88b48d43c77004a246f2dd0f12a68673
WizWiz <omidnaderivip@gmail.com>
2023-06-04 23:07:52 +0000

    update script

---
+++ update.sh
@@ -0,0 +1,300 @@
+#!/bin/bash
+
+# Written By: wizwiz
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "\033[33mPlease run as root\033[0m"
+    exit
+fi
+
+wait
+
+echo " "
+
+PS3=" Please Select Action: "
+options=("Update bot" "Update panel" "Backup" "Delete" "Donate" "Exit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"Update bot")
+			echo " "
+			read -p "Are you sure you want to update?[y/n]: " answer
+			echo " "
+			if [ "$answer" != "${answer#[Yy]}" ]; then
+			mv /var/www/html/wizwizxui-timebot/baseInfo.php /root/
+			mv /var/www/html/wizwizxui-timebot/userInfo.json /root/
+			sudo apt-get install -y git
+			sudo apt-get install -y wget
+			sudo apt-get install -y unzip
+			sudo apt install curl -y
+			echo -e "\n\e[92mUpdating ...\033[0m\n"
+			sleep 4
+			rm -r /var/www/html/wizwizxui-timebot/
+			echo -e "\n\e[92mWait a few seconds ...\033[0m\n"
+			sleep 3
+			git clone https://github.com/wizwizdev/wizwizxui-timebot.git /var/www/html/wizwizxui-timebot
+			sudo chown -R www-data:www-data /var/www/html/wizwizxui-timebot/
+			sudo chmod -R 755 /var/www/html/wizwizxui-timebot/
+			sleep 3
+			mv /root/baseInfo.php /var/www/html/wizwizxui-timebot/
+			mv /root/userInfo.json /var/www/html/wizwizxui-timebot/
+# 			if [ $? -ne 0 ]; then
+# 			echo -e "\n\e[41mError: The update failed!\033[0m\n"
+# 			exit 1
+# 			else
+			
+			sleep 1
+			
+			bot_token=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botToken' | cut -d"'" -f2)
+			bot_token2=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botToken' | cut -d'"' -f2)
+			bot_url=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botUrl' | cut -d'"' -f2)
+			
+			filepath="/var/www/html/wizwizxui-timebot/baseInfo.php"
+			
+			bot_value=$(cat $filepath | grep '$admin =' | sed 's/.*= //' | sed 's/;//')
+			
+			MESSAGE="🤖 WizWiz robot has been successfully updated!"
+			
+			curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id="${bot_value}" -d text="$MESSAGE"
+			
+			curl -s -X POST "https://api.telegram.org/bot${bot_token2}/sendMessage" -d chat_id="${bot_value}" -d text="$MESSAGE"
+			
+			sleep 1
+        
+			url="${bot_url}install/install.php?updateBot"
+			curl $url
+			
+			sleep 1
+			
+			sudo rm -r /var/www/html/wizwizxui-timebot/webpanel
+			sudo rm -r /var/www/html/wizwizxui-timebot/install
+			rm /var/www/html/wizwizxui-timebot/createDB.php
+			
+			clear
+			
+			echo -e "\n\e[92mThe script was successfully updated!\033[0m\n"
+			
+# 			fi
+
+			else
+			  echo -e "\e[41mCancel the update.\033[0m\n"
+			fi
+
+			break ;;
+		
+		"Update panel")
+			echo " "
+			read -p "Are you sure you want to update?[y/n]: " answer
+			echo " "
+			if [ "$answer" != "${answer#[Yy]}" ]; then
+			
+			sudo apt-get install -y php-ssh2
+			sudo apt-get install -y libssh2-1-dev libssh2-1
+
+			destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+
+			if [ -z "$destination_dir" ]; then
+			    RANDOM_NUMBER=$(( RANDOM % 10000000 + 1000000 ))
+			    mkdir "/var/www/html/wizpanel${RANDOM_NUMBER}"
+			    echo "Directory created: wizpanel${RANDOM_NUMBER}"
+			    echo "Folder created successfully!"
+			    sudo mkdir /root/updatewizwiz
+   			    sleep 1
+			    touch /root/updatewizwiz/wizup.txt
+			    sleep 1
+			    ASAS="$"
+			    echo "${ASAS}path = '${RANDOM_NUMBER}';" >> /root/updatewizwiz/wizup.txt
+			else
+			    echo "Folder already exists."
+			fi
+			
+			
+
+			 destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+
+			 cd /var/www/html/
+			 wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/7.5.3/wizwizpanel.zip
+
+			 file_to_transfer="/var/www/html/wizwizpanel.zip"
+			 destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+
+			 if [ -z "$destination_dir" ]; then
+			   echo "Error: Could not find directory containing 'wiz' in '/var/www/html'"
+			   exit 1
+			 fi
+
+			 mv "$file_to_transfer" "$destination_dir/" && yes | unzip "$destination_dir/wizwizpanel.zip" -d "$destination_dir/" && rm "$destination_dir/wizwizpanel.zip" && sudo chmod -R 755 "$destination_dir/" && sudo chown -R www-data:www-data "$destination_dir/" 
+
+
+			wait
+
+
+			echo -e "\n\e[92mUpdating ...\033[0m\n"
+			
+			bot_token=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botToken' | cut -d"'" -f2)
+			
+			filepath="/var/www/html/wizwizxui-timebot/baseInfo.php"
+			
+			bot_value=$(cat $filepath | grep '$admin =' | sed 's/.*= //' | sed 's/;//')
+			
+			MESSAGE="🕹 WizWiz panel has been successfully updated!"
+
+			curl -s -X POST "https://api.telegram.org/bot${bot_token}/sendMessage" -d chat_id="${bot_value}" -d text="$MESSAGE"
+			
+			sleep 1
+			
+			if [ $? -ne 0 ]; then
+			echo -e "\n\e[41mError: The update failed!\033[0m\n"
+			exit 1
+			else
+			
+# 			echo -e '\e[31m'
+
+# 			find /var/www/html -type d -name "*wizpanel*" -print | sed "s|/var/www/html|& \n\n\nPanel: https://yourdomain.com|g"
+			
+# 			echo -e '\033[0m'
+
+
+
+
+			echo -e ' '
+			echo -e ' '
+
+			read -p "Enter the domain: " domainname
+			
+			if [ "$domainname" = "" ]; then
+
+			exit
+
+			else
+			
+			DOMAIN_NAME="$domainname"
+			
+			PATHS=$(cat /root/updatewizwiz/wizup.txt | grep '$path' | cut -d"'" -f2)
+		
+			(crontab -l ; echo "* * * * * curl https://${DOMAIN_NAME}/wizpanel${PATHS}/backupnutif.php >/dev/null 2>&1") | sort - | uniq - | crontab -
+			fi
+			
+			
+
+			echo -e ' '
+			echo -e ' '
+
+
+			echo -e "\e[92mPanel: \e[31mhttps://${DOMAIN_NAME}/wizpanel${PATHS}\033[0m\n"
+			
+		
+			echo -e "\e[92mThe script was successfully updated!\033[0m\n"
+			
+			fi
+
+
+
+
+			else
+			  echo -e "\e[41mCancel the update.\033[0m\n"
+			fi
+
+			break ;;
+		"Backup")
+			echo " "
+			
+			wait
+
+			BOT_TOKEN=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botToken' | cut -d"'" -d'"' -f2)
+			ROOT_USER=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$dbUserName' | cut -d"'" -f2)
+			ROOT_PASSWORD=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$dbPassword' | cut -d"'" -f2)
+			BOT_URL=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$botUrl' | cut -d"'" -f2)
+			
+			filepath="/var/www/html/wizwizxui-timebot/baseInfo.php"
+			ADMIN_ID=$(cat $filepath | grep '$admin =' | sed 's/.*= //' | sed 's/;//')
+			
+			echo "SELECT 1" | mysql -u$ROOT_USER -p$ROOT_PASSWORD 2>/dev/null
+
+			sleep 1
+			ASAS="$"
+			if [ $? -eq 0 ]; then
+
+			touch /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+
+			chmod -R 777 /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+
+			echo " " >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "<?php" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "include 'settings/jdf.php';" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "function sendDocument(${ASAS}username, ${ASAS}document_path, ${ASAS}caption = null, ${ASAS}parse_mode = 'HTML') {" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "${ASAS}url = 'https://api.telegram.org/bot${BOT_TOKEN}/sendDocument';" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "${ASAS}wizwiz = ['chat_id' => ${ASAS}username,'document' => new CURLFile(${ASAS}document_path),'caption' => ${ASAS}caption,'parse_mode' => ${ASAS}parse_mode];" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "${ASAS}ch = curl_init();" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "curl_setopt_array(${ASAS}ch, [CURLOPT_URL => ${ASAS}url,CURLOPT_RETURNTRANSFER => true,CURLOPT_POSTFIELDS => ${ASAS}wizwiz]);" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "${ASAS}result = curl_exec(${ASAS}ch);curl_close(${ASAS}ch);return ${ASAS}result;}" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "date_default_timezone_set('Asia/Tehran');${ASAS}date = jdate('Y-m-d | H:i:s');" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "sendDocument('${ADMIN_ID}', '${BOT_URL}wizwiz.sql', '❤️ db '.${ASAS}date);" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo "?>" >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			echo " " >> /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+
+			DB_NAME=wizwiz
+			backup_path="/var/www/html/wizwizxui-timebot/"
+			backup_filesql="$backup_path$DB_NAME.sql"
+			mysqldump --user=$ROOT_USER --password=$ROOT_PASSWORD --host=localhost wizwiz > $backup_filesql
+			
+			clear
+			
+			sleep 0.5
+			
+			url="${BOT_URL}backup-wizwiz.php"
+
+			curl $url
+			
+			sleep 2
+						
+			rm /var/www/html/wizwizxui-timebot/backup-wizwiz.php
+			rm /var/www/html/wizwizxui-timebot/wizwiz.sql
+			
+			
+			echo -e "\xE2\x98\x85 \e[94mThe backup settings have been successfully completed.\033[0m\n"
+			
+			else
+			    echo "ERROR: MySQL password is incorrect"
+			fi
+
+			break ;;
+		"Delete")
+			echo " "
+			
+			wait
+			
+			passs=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$pass' | cut -d"'" -f2)
+   			userrr=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$user' | cut -d"'" -f2)
+			pathsss=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$path' | cut -d"'" -f2)
+			passsword=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$dbPassword' | cut -d"'" -f2)
+   			userrrname=$(cat /var/www/html/wizwizxui-timebot/baseInfo.php | grep '$dbUserName' | cut -d"'" -f2)
+			
+			mysql -u $userrr -p$passs -e "DROP DATABASE wizwiz;" -e "DROP USER '$userrrname'@'localhost';" -e "DROP USER '$userrrname'@'%';"
+			
+			clear
+
+			sudo rm -r /var/www/html/wizwizxui-timebot/wizpanel${pathsss}
+			sudo rm -r /var/www/html/wizwizxui-timebot
+			
+			sleep 1
+			
+			(crontab -l | grep -v "messagewizwiz.php") | crontab -
+			(crontab -l | grep -v "rewardReport.php") | crontab -
+			(crontab -l | grep -v "warnusers.php") | crontab -
+			(crontab -l | grep -v "backupnutif.php") | crontab -
+			
+			echo -e "\n\xE2\x98\x85 \e[94mRemoved successfully.\033[0m\n"
+			break ;;
+		"Donate")
+			echo " "
+			echo -e "\n\e[91mBanksepah ( toran ): \e[36m5892101222351344\033[0m\n\e[91mTron(trx): \e[36mTY8j7of18gbMtneB8bbL7SZk5gcntQEemG\n\e[91mBitcoin: \e[36mbc1qcnkjnqvs7kyxvlfrns8t4ely7x85dhvz5gqge4\033[0m\n"
+			exit 0
+			break ;;
+		"Exit")
+			echo " "
+			break
+			;;
+			*) echo "Invalid option!"
+	esac
+done
---
+++ wizwiz.sh
@@ -0,0 +1,376 @@
+#!/bin/bash
+
+# Written By: wizwiz
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "\033[33mPlease run as root\033[0m"
+    exit
+fi
+
+wait
+
+echo -e "\e[32m
+██     ██ ██ ███████ ██     ██ ██ ███████     ██   ██ ██    ██ ██ 
+██     ██ ██    ███  ██     ██ ██    ███       ██ ██  ██    ██ ██ 
+██  █  ██ ██   ███   ██  █  ██ ██   ███         ███   ██    ██ ██ 
+██ ███ ██ ██  ███    ██ ███ ██ ██  ███         ██ ██  ██    ██ ██ 
+ ███ ███  ██ ███████  ███ ███  ██ ███████     ██   ██  ██████  ██ 
+\033[0m"
+echo -e "    \e[31mTelegram Channel: \e[34m@wizwizch\033[0m | \e[31mTelegram Group: \e[34m@wizwizdev\033[0m\n"
+
+#sleep
+echo -e "\e[32mInstalling WizWiz script ... \033[0m\n"
+sleep 5
+
+sudo apt update && apt upgrade -y
+echo -e "\e[92mThe server was successfully updated ...\033[0m\n"
+
+
+PKG=(
+    lamp-server^
+    libapache2-mod-php 
+    mysql-server 
+    apache2 
+    php-mbstring 
+    php-zip 
+    php-gd 
+    php-json 
+    php-curl 
+#     phpmyadmin
+)
+
+for i in "${PKG[@]}"
+do
+    dpkg -s $i &> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "$i is already installed"
+    else
+        apt install $i -y
+        if [ $? -ne 0 ]; then
+            echo "Error installing $i"
+            exit 1
+        fi
+    fi
+done
+
+echo -e "\n\e[92mPackages Installed Continuing ...\033[0m\n"
+
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password wizwizhipass' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password wizwizhipass' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password wizwizhipass' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+sudo apt-get install phpmyadmin -y
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
+sudo a2enconf phpmyadmin.conf
+sudo systemctl restart apache2
+
+wait
+
+sudo apt-get install -y php-soap
+
+sudo apt-get install libapache2-mod-php
+
+# extension=soap.so
+# echo "extension=soap.so" >> /usr/local/lib/php.ini
+# sed -i 's/;extension=soap/extension=soap/g' /usr/local/lib/php.ini
+
+
+# services
+sudo systemctl enable mysql.service
+sudo systemctl start mysql.service
+sudo systemctl enable apache2
+sudo systemctl start apache2
+
+echo -e "\n\e[92m Setting Up UFW...\033[0m\n"
+
+ufw allow 'Apache'
+
+sudo systemctl restart apache2
+
+echo -e "\n\e[92mInstalling ...\033[0m\n"
+
+sleep 1
+
+sudo apt-get install -y git
+sudo apt-get install -y wget
+sudo apt-get install -y unzip
+sudo apt install curl -y
+sudo apt-get install -y php-ssh2
+sudo apt-get install -y libssh2-1-dev libssh2-1
+sudo systemctl restart apache2.service
+
+wait
+
+git clone https://github.com/wizwizdev/wizwizxui-timebot.git /var/www/html/wizwizxui-timebot
+sudo chown -R www-data:www-data /var/www/html/wizwizxui-timebot/
+sudo chmod -R 755 /var/www/html/wizwizxui-timebot/
+echo -e "\n\033[33mWizWiz config and script have been installed successfully\033[0m"
+
+wait
+    
+    
+destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+    
+if [ -z "$destination_dir" ]; then
+    RANDOM_NUMBER=$(( RANDOM % 10000000 + 1000000 ))
+    mkdir "/var/www/html/wizpanel${RANDOM_NUMBER}"
+    echo "Directory created: wizpanel${RANDOM_NUMBER}"
+    echo "Folder created successfully!"
+else
+    echo "Folder already exists."
+fi
+   
+ destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+
+ cd /var/www/html/
+ wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/7.5.3/wizwizpanel.zip
+
+ file_to_transfer="/var/www/html/wizwizpanel.zip"
+ destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
+
+ if [ -z "$destination_dir" ]; then
+   echo "Error: Could not find directory containing 'wiz' in '/var/www/html'"
+   exit 1
+ fi
+
+ mv "$file_to_transfer" "$destination_dir/" && yes | unzip "$destination_dir/wizwizpanel.zip" -d "$destination_dir/" && rm "$destination_dir/wizwizpanel.zip" && sudo chmod -R 755 "$destination_dir/" && sudo chown -R www-data:www-data "$destination_dir/" 
+
+
+wait
+
+
+if [ ! -d "/root/confwizwiz" ]; then
+
+    sudo mkdir /root/confwizwiz
+    
+    sleep 1
+    
+    touch /root/confwizwiz/dbrootwizwiz.txt
+    
+    sleep 1
+    
+    randomdbpasstxt=$(openssl rand -base64 10 | tr -dc 'a-zA-Z0-9' | cut -c1-8)
+
+    ASAS="$"
+
+    echo "${ASAS}user = 'root';" >> /root/confwizwiz/dbrootwizwiz.txt
+    echo "${ASAS}pass = '${randomdbpasstxt}';" >> /root/confwizwiz/dbrootwizwiz.txt
+    echo "${ASAS}path = '${RANDOM_NUMBER}';" >> /root/confwizwiz/dbrootwizwiz.txt
+    
+    sleep 1
+
+    passs=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$pass' | cut -d"'" -f2)
+    userrr=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$user' | cut -d"'" -f2)
+
+    sudo mysql -u $userrr -p$passs -e "alter user '$userrr'@'localhost' identified with mysql_native_password by '$passs';FLUSH PRIVILEGES;"
+
+    echo "SELECT 1" | mysql -u$userrr -p$passs 2>/dev/null
+
+    echo "Folder created successfully!"
+else
+    echo "Folder already exists."
+fi
+
+clear
+
+echo " "
+echo -e "\e[32m
+██     ██ ██ ███████ ██     ██ ██ ███████     ███████ ███████ ██      
+██     ██ ██    ███  ██     ██ ██    ███      ██      ██      ██      
+██  █  ██ ██   ███   ██  █  ██ ██   ███       ███████ ███████ ██      
+██ ███ ██ ██  ███    ██ ███ ██ ██  ███             ██      ██ ██      
+ ███ ███  ██ ███████  ███ ███  ██ ███████     ███████ ███████ ███████ 
+\033[0m\n"
+
+read -p "Enter the domain: " domainname
+if [ "$domainname" = "" ]; then
+
+echo -e "\n\033[91mPlease wait ...\033[0m\n"
+sleep 3
+
+echo -e "\e[36mNothing was registered for the domain.\033[0m\n"
+
+echo -e "\n\033[0m Good Luck Baby\n"
+
+else
+# variables
+DOMAIN_NAME="$domainname"
+# WILDCARD_DOMAIN="*.$wildcarddomain"
+
+# update cron
+PATHS=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$path' | cut -d"'" -f2)
+(crontab -l ; echo "* * * * * curl https://${DOMAIN_NAME}/wizwizxui-timebot/settings/messagewizwiz.php >/dev/null 2>&1") | sort - | uniq - | crontab -
+(crontab -l ; echo "* * * * * curl https://${DOMAIN_NAME}/wizwizxui-timebot/settings/rewardReport.php >/dev/null 2>&1") | sort - | uniq - | crontab -
+(crontab -l ; echo "* * * * * curl https://${DOMAIN_NAME}/wizwizxui-timebot/settings/warnusers.php >/dev/null 2>&1") | sort - | uniq - | crontab -
+(crontab -l ; echo "* * * * * curl https://${DOMAIN_NAME}/wizpanel${PATHS}/backupnutif.php >/dev/null 2>&1") | sort - | uniq - | crontab -
+
+echo -e "\n\e[92m Setting Up Cron...\033[0m\n"
+
+# Allow HTTP and HTTPS traffic
+echo -e "\n\033[1;7;31mAllowing HTTP and HTTPS traffic...\033[0m\n"
+sudo ufw allow 80
+sudo ufw allow 443
+
+# Let's Encrypt
+echo -e "\n\033[1;7;32mInstalling Let's Encrypt...\033[0m\n"
+sudo apt install letsencrypt -y
+
+# automatic certificate renewal
+echo -e "\n\033[1;7;33mEnabling automatic certificate renewal...\033[0m\n"
+sudo systemctl enable certbot.timer
+
+# SSL certificate using standalone mode
+echo -e "\n\033[1;7;34mObtaining SSL certificate using standalone mode...\033[0m\n"
+sudo certbot certonly --standalone --agree-tos --preferred-challenges http -d $DOMAIN_NAME
+
+# Certbot Apache plugin
+echo -e "\n\033[1;7;35mInstalling Certbot Apache plugin...\033[0m\n"
+sudo apt install python3-certbot-apache -y
+
+# SSL certificate using Apache plugin
+echo -e "\n\033[1;7;36mObtaining SSL certificate using Apache plugin...\033[0m\n"
+sudo certbot --apache --agree-tos --preferred-challenges http -d $DOMAIN_NAME
+
+# SSL certificate using manual DNS mode (wildcard)
+# echo -e "\n\033[1;7;33mObtaining SSL certificate using manual DNS mode (wildcard)...\033[0m\n"
+# sudo certbot certonly --manual --agree-tos --preferred-challenges dns -d $DOMAIN_NAME -d $WILDCARD_DOMAIN
+
+
+echo -e "\e[32m======================================"
+echo -e "SSL certificate obtained successfully!"
+echo -e "======================================\033[0m"
+
+
+wait
+
+echo " "
+
+ROOT_PASSWORD=$(cat /root/confwizwiz/dbrootwizwiz.txt | grep '$pass' | cut -d"'" -f2)
+ROOT_USER="root"
+echo "SELECT 1" | mysql -u$ROOT_USER -p$ROOT_PASSWORD 2>/dev/null
+
+
+if [ $? -eq 0 ]; then
+
+wait
+
+    randomdbpass=$(openssl rand -base64 10 | tr -dc 'a-zA-Z0-9' | cut -c1-8)
+
+    randomdbdb=$(openssl rand -base64 10 | tr -dc 'a-zA-Z' | cut -c1-8)
+
+    if [[ $(mysql -u root -p$ROOT_PASSWORD -e "SHOW DATABASES LIKE 'wizwiz'") ]]; then
+        clear
+        echo -e "\n\e[91mYou have already created the database\033[0m\n"
+    else
+        dbname=wizwiz
+        clear
+        echo -e "\n\e[32mPlease enter the database username!\033[0m"
+        printf "[+] Default user name is \e[91m${randomdbdb}\e[0m ( let it blank to use this user name ): "
+        read dbuser
+        if [ "$dbuser" = "" ]; then
+        dbuser=$randomdbdb
+        fi
+
+        echo -e "\n\e[32mPlease enter the database password!\033[0m"
+        printf "[+] Default user name is \e[91m${randomdbpass}\e[0m ( let it blank to use this user name ): "
+        read dbpass
+        if [ "$dbpass" = "" ]; then
+        dbpass=$randomdbpass
+        fi
+
+        mysql -u root -p$ROOT_PASSWORD -e "CREATE DATABASE $dbname;" -e "CREATE USER '$dbuser'@'%' IDENTIFIED WITH mysql_native_password BY '$dbpass';GRANT ALL PRIVILEGES ON * . * TO '$dbuser'@'%';FLUSH PRIVILEGES;" -e "CREATE USER '$dbuser'@'localhost' IDENTIFIED WITH mysql_native_password BY '$dbpass';GRANT ALL PRIVILEGES ON * . * TO '$dbuser'@'localhost';FLUSH PRIVILEGES;"
+        
+        echo -e "\n\e[95mDatabase Created.\033[0m"
+        
+        wait
+        
+
+
+        printf "\n\e[33m[+] \e[36mBot Token: \033[0m"
+        read YOUR_BOT_TOKEN
+        printf "\e[33m[+] \e[36mChat id: \033[0m"
+        read YOUR_CHAT_ID
+        printf "\e[33m[+] \e[36mDomain: \033[0m"
+        read YOUR_DOMAIN
+        echo " "
+        if [ "$YOUR_BOT_TOKEN" = "" ] || [ "$YOUR_DOMAIN" = "" ] || [ "$YOUR_CHAT_ID" = "" ]; then
+           exit
+        fi
+
+        ASAS="$"
+	
+        wait
+
+        sleep 1
+        
+        file_path="/var/www/html/wizwizxui-timebot/baseInfo.php"
+        
+        if [ -f "$file_path" ]; then
+          rm "$file_path"
+          echo -e "File deleted successfully."
+        else
+          echo -e "File not found."
+        fi
+        
+        sleep 2
+        
+        # print file
+        echo -e "<?php" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "error_reporting(0);" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}botToken = '${YOUR_BOT_TOKEN}';" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}dbUserName = '${dbuser}';" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}dbPassword = '${dbpass}';" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}dbName = '${dbname}';" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}botUrl = 'https://${YOUR_DOMAIN}/wizwizxui-timebot/';" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "${ASAS}admin = ${YOUR_CHAT_ID};" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+        echo -e "?>" >> /var/www/html/wizwizxui-timebot/baseInfo.php
+
+        sleep 1
+
+        curl -F "url=https://${YOUR_DOMAIN}/wizwizxui-timebot/bot.php" "https://api.telegram.org/bot${YOUR_BOT_TOKEN}/setWebhook"
+        MESSAGE="✅ The wizwiz bot has been successfully installed! @wizwizch"
+        curl -s -X POST "https://api.telegram.org/bot${YOUR_BOT_TOKEN}/sendMessage" -d chat_id="${YOUR_CHAT_ID}" -d text="$MESSAGE"
+        
+        
+        sleep 1
+        
+        url="https://${YOUR_DOMAIN}/wizwizxui-timebot/createDB.php"
+        curl $url
+        
+        sleep 1
+        
+        sudo rm -r /var/www/html/wizwizxui-timebot/webpanel
+	    sudo rm -r /var/www/html/wizwizxui-timebot/install
+	    sudo rm /var/www/html/wizwizxui-timebot/createDB.php
+            
+        clear
+        
+        echo " "
+        
+        echo -e "\e[100mDatabase information:\033[0m"
+        echo -e "\e[33mDatabase name: \e[36m${dbname}\033[0m"
+        echo -e "\e[33mDatabase username: \e[36m${dbuser}\033[0m"
+        echo -e "\e[33mDatabase password: \e[36m${dbpass}\033[0m"
+        echo " "
+        echo -e "\e[100mwizwiz panel:\033[0m"
+        echo -e "\e[33maddres: \e[36mhttps://${YOUR_DOMAIN}/wizpanel${RANDOM_NUMBER}\033[0m"
+        echo -e "\e[33musername panel: \e[36madmin\033[0m"
+        echo -e "\e[33mpassword panel: \e[36madmin\033[0m\n"
+        
+        wait
+        
+        echo -e "Good Luck Baby! \e[94mThis project is for free. If you like it, be sure to donate me :) , so let's go \033[0m\n"
+
+        fi
+
+
+        elif [ "$ROOT_PASSWORD" = "" ] || [ "$ROOT_USER" = "" ]; then
+        echo -e "\n\e[36mThe password is empty.\033[0m\n"
+        else 
+        
+        echo -e "\n\e[36mThe password is not correct.\033[0m\n"
+
+        fi
+
+fi
