6a63a7c41a04c9468373c35805b4c71b7bf74102
Mohammadreza Ziadiparast <ziadiparast@gmail.com>
2023-06-15 17:10:14 +0330

    [Fix] baseInfo path in db.php

--- webpanel/includ/db.php
+++ webpanel/includ/db.php
@@ -1,13 +1,17 @@
-<!--
+<?php
+/*
 * WizWiz v7.5.3
 * https://github.com/wizwizdev/wizwizxui-timebot
 * Copyright (c) @wizwizch
--->
-<?php
-include '../wizwizxui-timebot-main/baseInfo.php';
+*/
+
+require __DIR__.'/../../baseInfo.php';
+
 $servername = "localhost";
 $conn = new mysqli($servername, $dbUserName, $dbPassword, $dbName);
 $conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
