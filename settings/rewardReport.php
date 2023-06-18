4707b37ac545870b487b5ddba7d91864e7f80821
WizWiz <omidnaderivip@gmail.com>
2023-06-19 01:23:25 +0330

    Update rewardReport.php

--- settings/rewardReport.php
+++ settings/rewardReport.php
@@ -2,25 +2,38 @@
 include_once '../baseInfo.php';
 include_once '../config.php';
 
-if(file_exists("botstate.json")){
-    $botState = json_decode(file_get_contents("botstate.json"),true);
-    $sellState=$botState['sellState']=="off"?"خاموش ❌":"روشن ✅";
-    $searchState=$botState['searchState']=="off"?"خاموش ❌":"روشن ✅";
-    $rewaredTime = ($botState['rewaredTime']??0);
-    $rewaredChannel = $botState['rewardChannel'];
 
-    if($rewaredTime>0 && $rewaredChannel != null){
-        $lastTime = $botState['lastRewardMessage']??0;
-        if(time() > $lastTime){
-            $time = time() - ($rewaredTime * 60 * 60);
-            $stmt = $connection->prepare("SELECT SUM(amount) as total FROM `orders_list` WHERE `date` > ?");
-            $stmt->bind_param("i", $time);
-            $stmt->execute();
-            $totalRewards = number_format($stmt->get_result()->fetch_assoc()['total']) . " تومان";
-            $stmt->close();
-            $botState['lastRewardMessage']=time() + ($rewaredTime * 60 * 60);
-            file_put_contents("botstate.json",json_encode($botState));
-            $txt = "⁮⁮ ⁮⁮ ⁮⁮ ⁮⁮
+$sellState=$botState['sellState']=="off"?"خاموش ❌":"روشن ✅";
+$searchState=$botState['searchState']=="off"?"خاموش ❌":"روشن ✅";
+$rewaredTime = ($botState['rewaredTime']??0);
+$rewaredChannel = $botState['rewardChannel'];
+
+if($rewaredTime>0 && $rewaredChannel != null){
+    $lastTime = $botState['lastRewardMessage']??0;
+    if(time() > $lastTime){
+        $time = time() - ($rewaredTime * 60 * 60);
+        $stmt = $connection->prepare("SELECT SUM(amount) as total FROM `orders_list` WHERE `date` > ?");
+        $stmt->bind_param("i", $time);
+        $stmt->execute();
+        $totalRewards = number_format($stmt->get_result()->fetch_assoc()['total']) . " تومان";
+        $stmt->close();
+        
+        $botState['lastRewardMessage']=time() + ($rewaredTime * 60 * 60);
+        
+        $stmt = $connection->prepare("SELECT * FROM `setting` WHERE `type` = 'BOT_STATES'");
+        $stmt->execute();
+        $isExists = $stmt->get_result();
+        $stmt->close();
+        if($isExists->num_rows>0) $query = "UPDATE `setting` SET `value` = ? WHERE `type` = 'BOT_STATES'";
+        else $query = "INSERT INTO `setting` (`type`, `value`) VALUES ('BOT_STATES', ?)";
+        $newData = json_encode($botState);
+        
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("s", $newData);
+        $stmt->execute();
+        $stmt->close();
+
+        $txt = "⁮⁮ ⁮⁮ ⁮⁮ ⁮⁮
 🔰درآمد من در $rewaredTime ساعت گذشته
 
 💰مبلغ : $totalRewards تومان
@@ -28,7 +41,6 @@ if(file_exists("botstate.json")){
 ☑️ $channelLock
 
 ";
-            sendMessage($txt, null, null, $rewaredChannel);
-        }
-    }    
-}
+        sendMessage($txt, null, null, $rewaredChannel);
+    }
+}    
