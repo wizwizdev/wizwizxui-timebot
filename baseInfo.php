170181cd0aa0d0848bf5ec4396f31e72ed312cdc
WizWiz <omidnaderivip@gmail.com>
2023-06-19 01:22:39 +0330

    Delete baseInfo.php

--- baseInfo.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  error_reporting(0);
-  $botToken = '[BOTTOKEN]';
-  $dbUserName = '[DBUSERNAME]';
-  $dbPassword = '[DBPASSWORD]';
-  $dbName = '[DBNAME]';
-  $admin = [ADMIN];
-  $botUrl = '[BOTURL]';
-?>
