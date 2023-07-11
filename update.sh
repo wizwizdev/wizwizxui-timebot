ec115e904f8251e57ce19070f31f74a48c237226
Aryan-Ebadi <82537103+Aryan-Ebadi@users.noreply.github.com>
2023-07-11 20:21:15 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -31,7 +31,7 @@ do
 			rm -r /var/www/html/wizwizxui-timebot/
 			echo -e "\n\e[92mWait a few seconds ...\033[0m\n"
 			sleep 3
-			git clone https://github.com/wizwizdev/wizwizxui-timebot.git /var/www/html/wizwizxui-timebot
+			git clone https://github.com/Aryan-Ebadi/vpnmasters-v2ray-bot.git /var/www/html/wizwizxui-timebot
 			sudo chown -R www-data:www-data /var/www/html/wizwizxui-timebot/
 			sudo chmod -R 755 /var/www/html/wizwizxui-timebot/
 			sleep 3
