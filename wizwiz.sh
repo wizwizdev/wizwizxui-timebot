cf3cd9cd2d480b2dbe7a9481e07360cd850ad46c
WizWiz <omidnaderivip@gmail.com>
2023-08-26 12:51:20 +0330

    Update wizwiz.sh

--- wizwiz.sh
+++ wizwiz.sh
@@ -124,7 +124,7 @@ fi
  destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
 
  cd /var/www/html/
- wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/7.5.3/wizwizpanel.zip
+ wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/8.1.1/wizwizpanel.zip
 
  file_to_transfer="/var/www/html/wizwizpanel.zip"
  destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
