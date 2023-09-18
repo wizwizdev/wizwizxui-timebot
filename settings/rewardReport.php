1fe70aca60b5dc9bff7d3b30f41d7b2482288772
WizWiz <27927279+wizwizdev@users.noreply.github.com>
2023-09-19 01:25:27 +0330

    Update rewardReport.php

--- settings/rewardReport.php
+++ settings/rewardReport.php
@@ -12,7 +12,8 @@ if($rewaredTime>0 && $rewaredChannel != null){
     $lastTime = $botState['lastRewardMessage']??0;
     if(time() > $lastTime){
         $time = time() - ($rewaredTime * 60 * 60);
-        $stmt = $connection->prepare("SELECT SUM(amount) as total FROM `orders_list` WHERE `date` > ?");
+        
+        $stmt = $connection->prepare("SELECT SUM(price) as total FROM `pays` WHERE `request_date` > ?");
         $stmt->bind_param("i", $time);
         $stmt->execute();
         $totalRewards = number_format($stmt->get_result()->fetch_assoc()['total']);
