03b66929fbb91092f1d357ab7ddc670e5228f0c1
WizWiz <27927279+wizwizdev@users.noreply.github.com>
2023-10-30 00:31:18 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -127,7 +127,7 @@ do
 			 destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
 
 			 cd /var/www/html/
-			 wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/8.2.2/wizwizpanel.zip
+			 wget -O wizwizpanel.zip https://github.com/wizwizdev/wizwizxui-timebot/releases/download/9.1.1/wizwizpanel.zip
 
 			 file_to_transfer="/var/www/html/wizwizpanel.zip"
 			 destination_dir=$(find /var/www/html -type d -name "*wizpanel*" | head -n 1)
