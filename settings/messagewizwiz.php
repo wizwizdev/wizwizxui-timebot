9b511520acd1d30ac0bb5c3f8ef1704d2668c259
WizWiz <27927279+wizwizdev@users.noreply.github.com>
2023-12-21 23:16:27 +0330

    Update messagewizwiz.php

--- settings/messagewizwiz.php
+++ settings/messagewizwiz.php
@@ -1,17 +1,14 @@
 <?php 
-$msgInfo = json_decode(file_get_contents("messagewizwiz.json"),true);
-$offset = $msgInfo['offset']??-1;
-$messageParam = json_decode($msgInfo['text']);
-$rateLimit = $msgInfo['rateLimit']??0;
-
 include_once '../baseInfo.php';
 include_once '../config.php';
 include_once 'jdf.php';
 
+$rateLimit = $botState['rateLimit']??0;
 if(time() > $rateLimit){
     $rate = json_decode(file_get_contents("https://api.changeto.technology/api/rate"),true)['result'];
     if(!empty($rate['USD'])) $botState['USDRate'] = $rate['USD'];
     if(!empty($rate['TRX'])) $botState['TRXRate'] = $rate['TRX'];
+    $botState['rateLimit'] = strtotime("+1 hour");
     
     $stmt = $connection->prepare("SELECT * FROM `setting` WHERE `type` = 'BOT_STATES'");
     $stmt->execute();
@@ -25,121 +22,116 @@ if(time() > $rateLimit){
     $stmt->bind_param("s", $newData);
     $stmt->execute();
     $stmt->close();
-    
-    $msgInfo['rateLimit'] = strtotime("+1 hour");
-    file_put_contents("messagewizwiz.json",json_encode($msgInfo));
-
 }
 
-
-if($offset == '-1') exit;
-
-if($offset == '0'){
-    if($messageParam->type == "forwardall") $msg = "عملیات هدایت همگانی شروع شد";
-    else $msg = "عملیات ارسال پیام همگانی شروع شد";
-    
-    bot('sendMessage',[
-        'chat_id'=>$admin,
-        'text'=>$msg
-        ]);
-}
-$stmt = $connection->prepare("SELECT * FROM `users`ORDER BY `id` LIMIT 50 OFFSET ?");
-$stmt->bind_param("i", $offset);
+$stmt = $connection->prepare("SELECT * FROM `send_list` WHERE `state` = 1 LIMIT 1");
 $stmt->execute();
-$usersList = $stmt->get_result();
+$list = $stmt->get_result();
 $stmt->close();
 
-if( $usersList->num_rows > 1 ) {
-    while($user = $usersList->fetch_assoc()){
-        if($messageParam->type == 'text'){
-            sendMessage($messageParam->value,json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ]),null,$user['userid']);
-        }elseif($messageParam->type == 'music'){
-            bot('sendAudio',[
-                'chat_id' => $user['userid'],
-                'audio' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]);
-        }elseif($messageParam->type == 'video'){
-            bot('sendVideo',[
-                'chat_id' => $user['userid'],
-                'video' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]);
-        }elseif($messageParam->type == 'voice'){
-            bot('sendVoice',[
-                'chat_id' => $user['userid'],
-                'voice' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]);
-        }elseif($messageParam->type == 'document'){
-            bot('sendDocument',[
-                'chat_id' => $user['userid'],
-                'document' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]);
-        }elseif($messageParam->type == 'photo'){
-            bot('sendPhoto', [
-                'chat_id' => $user['userid'],
-                'photo' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]); 
-        }elseif($messageParam->type == "forwardall"){
-            forwardmessage($user['userid'], $messageParam->chat_id, $messageParam->message_id);
-        }
-        else {
-            bot('sendDocument',[
-                'chat_id' => $user['userid'],
-                'document' => $messageParam->value->fileid,
-                'caption' => $messageParam->value->caption,
-                'reply_markup'=>json_encode([
-                        'inline_keyboard' => [
-                            [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
-                            ]
-                    ])
-            ]);
-        }
-        $offset++;
-    }
-    $msgInfo['offset'] = $offset;
-    file_put_contents("messagewizwiz.json",json_encode($msgInfo));
-}else{
-    if($messageParam->type == "forwardall") $msg = "عملیات هدایت همگانی با موفقیت انجام شد";
-    else $msg = "عملیات ارسال پیام همگانی با موفقیت انجام شد";
+if($list->num_rows > 0){
+    $info = $list->fetch_assoc();
     
-    bot('sendMessage',[
-        'chat_id'=>$admin,
-        'text'=>$msg . "\nبه " . $offset . " نفر پیامتو فرستادم"
-        ]);
-    $msgInfo['offset'] = -1;
-    file_put_contents("messagewizwiz.json",json_encode($msgInfo));
+    $sendId = $info['id'];
+    $offset = $info['offset'];
+    $type = $info['type'];
+    $file_id = $info['file_id'];
+    $chat_id = $info['chat_id'];
+    $text = $info['text'];
+    $message_id = $info['message_id'];
+    
+    if($offset == '0'){
+        if($type == "forwardall") $msg = "عملیات هدایت همگانی شروع شد";
+        else $msg = "عملیات ارسال پیام همگانی شروع شد";
+        
+        bot('sendMessage',[
+            'chat_id'=>$admin,
+            'text'=>$msg
+            ]);
+    }
+    
+    $stmt = $connection->prepare("SELECT * FROM `users`ORDER BY `id` LIMIT 50 OFFSET ?");
+    $stmt->bind_param("i", $offset);
+    $stmt->execute();
+    $usersList = $stmt->get_result();
+    $stmt->close();
+    
+    $keys = json_encode([
+                'inline_keyboard' => [
+                    [['text'=>$buttonValues['start_bot'],'callback_data'=>"mainMenu"]]
+                    ]
+            ]);
+    if($usersList->num_rows > 1) {
+        while($user = $usersList->fetch_assoc()){
+            if($type == 'text'){
+                sendMessage($text,$keys,null,$user['userid']);
+            }elseif($type == 'music'){
+                bot('sendAudio',[
+                    'chat_id' => $user['userid'],
+                    'audio' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$keys
+                ]);
+            }elseif($type == 'video'){
+                bot('sendVideo',[
+                    'chat_id' => $user['userid'],
+                    'video' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$keys
+                ]);
+            }elseif($type == 'voice'){
+                bot('sendVoice',[
+                    'chat_id' => $user['userid'],
+                    'voice' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$keys
+                ]);
+            }elseif($type == 'document'){
+                bot('sendDocument',[
+                    'chat_id' => $user['userid'],
+                    'document' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$kes
+                ]);
+            }elseif($type == 'photo'){
+                bot('sendPhoto', [
+                    'chat_id' => $user['userid'],
+                    'photo' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$keys
+                ]); 
+            }elseif($type == "forwardall"){
+                forwardmessage($user['userid'], $chat_id, $message_id);
+            }
+            else {
+                bot('sendDocument',[
+                    'chat_id' => $user['userid'],
+                    'document' => $file_id,
+                    'caption' => $text,
+                    'reply_markup'=>$keys
+                ]);
+            }
+            $offset++;
+        }
+        $stmt = $connection->prepare("UPDATE `send_list` SET `offset` = ? WHERE `id` = ?");
+        $stmt->bind_param("ii", $offset, $sendId);
+        $stmt->execute();
+        $stmt->close();
+    }else{
+        if($type == "forwardall") $msg = "عملیات هدایت همگانی با موفقیت انجام شد";
+        else $msg = "عملیات ارسال پیام همگانی با موفقیت انجام شد";
+        
+        bot('sendMessage',[
+            'chat_id'=>$admin,
+            'text'=>$msg . "\nبه " . $offset . " نفر پیامتو فرستادم"
+            ]);
+            
+        $stmt = $connection->prepare("DELETE FROM `send_list` WHERE `id` = ?");
+        $stmt->bind_param('i', $sendId);
+        $stmt->execute();
+        $stmt->close();
+    }
 }
 
+
+?>
